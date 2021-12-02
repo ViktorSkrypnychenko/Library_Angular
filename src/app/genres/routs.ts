c55c6ca71a473ab51c9796e4b60767e9d62f8488
ViktorSkrypnychenko <92309069+ViktorSkrypnychenko@users.noreply.github.com>
2021-12-02 15:28:17 +0200

    Add files via upload

---
+++ src/app/genres/routs.ts
@@ -0,0 +1,7 @@
+import { AddGenreComponent, DisplayGenresComponent, EditGenreComponent } from "./index";
+
+export const genreRoute = [
+    {path: "add-genre", component: AddGenreComponent},
+    {path: "display-genres", component: DisplayGenresComponent},
+    {path: "edit-genre", component: EditGenreComponent}
+];
