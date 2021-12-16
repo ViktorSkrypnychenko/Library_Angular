b6fa90fe5eb55ecfa6454942552f548c73ecc5f1
ViktorSkrypnychenko <92309069+ViktorSkrypnychenko@users.noreply.github.com>
2021-12-16 10:35:21 +0200

    Delete src/app/library directory

--- src/app/library/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./add-author/add-author.component";
-export * from "./display-authors/display-authors.component";
-export * from "./edit-author/edit-author.component";
--- src/app/library/routs.ts
+++
@@ -1,7 +0,0 @@
-import { AddAuthorComponent, DisplayAuthorsComponent,EditAuthorComponent } from "./index";
-
-export const authorRoute = [
-    {path: "add-author", component: AddAuthorComponent},
-    {path: "display-authors", component: DisplayAuthorsComponent},
-    {path: "edit-author", component: EditAuthorComponent}
-];
