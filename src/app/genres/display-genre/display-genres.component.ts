fdb775e58df81a04c30b7851fccd13c60a575339
ViktorSkrypnychenko <92309069+ViktorSkrypnychenko@users.noreply.github.com>
2021-12-16 10:49:03 +0200

    add type

--- src/app/genres/display-genre/display-genres.component.ts
+++ src/app/genres/display-genre/display-genres.component.ts
@@ -1,28 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { LibraryServiceService } from 'src/app/library-service.service';
-
+import { Genre } from 'src/app/genre-book-author';
 
 @Component({
   selector: 'app-display-genres',
   templateUrl: './display-genres.component.html',
   styleUrls: ['./display-genres.component.css']
 })
+
 export class DisplayGenresComponent implements OnInit {
-  genreList:any;
+  
+  genreList: Genre[] = [];
 
   constructor(private libraryService:LibraryServiceService) { }
 
-  getGenre(){
-    this.libraryService.getGenre().subscribe(data=>{
-      this.genreList = data;
-    })
-  }
-  sortingAZ(){
-    this.genreList.sort((a: any,b: any)=>a.genre.localeCompare(b.genre));
-  }
-
   ngOnInit(): void {
     this.getGenre();
+  }
+
+  getGenre():void{
+    this.libraryService.getGenre().subscribe(data=>{
+      this.genreList = data
+    })
+  }
+
+  sortingAZ(){
+    this.genreList.sort((a: any,b: any)=>a.genre.localeCompare(b.genre));
   }
 
 }
