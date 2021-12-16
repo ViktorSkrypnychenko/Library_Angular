8a4c115998ec8d6cccc7bf42fc12e56a03ab1051
ViktorSkrypnychenko <92309069+ViktorSkrypnychenko@users.noreply.github.com>
2021-12-16 10:45:13 +0200

    add type

--- src/app/books/add-book/add-book.component.ts
+++ src/app/books/add-book/add-book.component.ts
@@ -1,33 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 import { LibraryServiceService } from 'src/app/library-service.service';
 import { FormControl, Validators, FormGroup } from '@angular/forms';
+import { Book,Genre,Author } from 'src/app/genre-book-author';
 
 @Component({
   selector: 'app-add-book',
   templateUrl: './add-book.component.html',
   styleUrls: ['./add-book.component.css']
 })
-export class AddBookComponent implements OnInit {
-  bookList:any;
-  genreList:any;
-  authorsList:any;
-  newBook:any[]=[];
-  addBookForm:any= FormGroup;
-  successfull=true;
 
+export class AddBookComponent implements OnInit {
+  
+  bookList:Book[]=[];
+  genreList:Genre[]=[];
+  authorsList:Author[]=[];
+  newBook:Book[]=[];
+  addBookForm!:FormGroup;
+  successfull:boolean=true;
 
   constructor(private libraryService:LibraryServiceService) { }
+
+  ngOnInit(): void {
+    this.addBookForm = new FormGroup({
+            surname: new FormControl('',[Validators.required,Validators.pattern(/[A-Za-z]/)]),
+            genre: new FormControl('',[Validators.required,Validators.pattern(/[A-Za-z]/)]),
+            title: new FormControl('',[Validators.required,Validators.pattern(/[A-Za-z]/)]),
+            pages: new FormControl('',[Validators.required,Validators.pattern(/^\d+$/)]),
+          });
+    this.getBookList();
+    this.getGenreList();
+    this.getAuthorList();
+  }
 
   getBookList(){
     this.libraryService.getBooks().subscribe(data =>{
       this.bookList = data
     })
   }
+
   getGenreList(){
     this.libraryService.getGenre().subscribe(data=>{
       this.genreList = data
     })
   }
+
   getAuthorList(){
     this.libraryService.getAuthor().subscribe(data=>{
       this.authorsList = data
@@ -36,30 +52,16 @@ export class AddBookComponent implements OnInit {
   
   addBook(){
     this.newBook.push(this.addBookForm.value);
-    this.newBook[0].authorId = Number(this.addBookForm.controls.surname.value.replace(/[^0-9]/g, ''));
-    this.newBook[0].authorSurname = this.addBookForm.controls.surname.value.replace(/[^a-zа-яё]/gi, '');
-    this.newBook[0].genreId = Number(this.addBookForm.controls.genre.value.replace(/[^0-9]/g, ''));
-    this.newBook[0].genre = this.addBookForm.controls.genre.value.replace(/[^a-zа-яё]/gi, '');
-    this.libraryService.addBook(this.newBook[0].authorSurname, this.newBook[0].title, this.newBook[0].genre, this.newBook[0].pages,this.newBook[0].authorId, this.newBook[0].genreId).subscribe(data=>{
+    this.newBook[0].authorId = Number(this.addBookForm.value.surname.replace(/[^0-9]/g, ''));
+    this.newBook[0].author = this.addBookForm.value.surname.replace(/[^a-zа-яё]/gi, '');
+    this.newBook[0].genreId = Number(this.addBookForm.value.genre.replace(/[^0-9]/g, ''));
+    this.newBook[0].genre = this.addBookForm.value.genre.replace(/[^a-zа-яё]/gi, '');
+    this.libraryService.addBook(this.newBook[0].author, this.newBook[0].title, this.newBook[0].genre, this.newBook[0].pages,this.newBook[0].authorId, this.newBook[0].genreId).subscribe(data=>{
       this.bookList.push(data);
       this.successfull=false
     })
     this.addBookForm.reset();
     this.newBook.length = 0;
-    console.log(this.bookList);
-    
-  }
-
-  ngOnInit(): void {
-    this.addBookForm = new FormGroup({
-            surname: new FormControl(''),
-            genre: new FormControl(''),
-            title: new FormControl('',[Validators.required,Validators.pattern(/[A-Za-z]/)]),
-            pages: new FormControl('',[Validators.required,Validators.pattern(/^\d+$/)]),
-          });
-    this.getBookList();
-    this.getGenreList();
-    this.getAuthorList();
   }
 
 }
