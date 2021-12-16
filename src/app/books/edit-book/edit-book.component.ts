60435855445a50edeaa9be162d9374ef0a470479
ViktorSkrypnychenko <92309069+ViktorSkrypnychenko@users.noreply.github.com>
2021-12-16 10:46:49 +0200

    add type

--- src/app/books/edit-book/edit-book.component.ts
+++ src/app/books/edit-book/edit-book.component.ts
@@ -1,38 +1,54 @@
 import { Component, OnInit } from '@angular/core';
 import { LibraryServiceService } from 'src/app/library-service.service';
 import { FormControl, Validators, FormGroup } from '@angular/forms';
+import { Book,Genre,Author } from 'src/app/genre-book-author';
 
 @Component({
   selector: 'app-edit-book',
   templateUrl: './edit-book.component.html',
   styleUrls: ['./edit-book.component.css']
 })
-export class EditBookComponent implements OnInit {
-  bookList:any;
-  genreList:any;
-  authorsList:any;
-  newBook:any[]=[];
-  bookForm:any= FormGroup;
-  successfull=true;
-  searchBookRes:any[]=[];
-  searchAlarm = true;
-  searchResult = false;
-  activationForm = true;
-  editingBook:any;
-  displayBooks = true;
 
+export class EditBookComponent implements OnInit {
+  bookList:Book[]=[];
+  genreList:Genre[]=[];
+  authorsList:Author[]=[];
+  newBook:Book[]=[];
+  bookForm!:FormGroup;
+  successfull=true;
+  searchBookRes:Book[]=[];
+  searchAlarm:boolean = true;
+  searchResult:boolean = false;
+  activationForm:boolean = true;
+  editingBook:Book[]=[];
+  displayBooks:boolean = true;
+ 
   constructor(private libraryService:LibraryServiceService) { }
+
+  ngOnInit(): void {
+    this.bookForm = new FormGroup({
+      surname: new FormControl(null,[Validators.pattern(/[A-Za-z]/)]),
+      genre: new FormControl(null,[Validators.pattern(/[A-Za-z]/)]),
+      title: new FormControl(null,[Validators.pattern(/[A-Za-z]/)]),
+      pages: new FormControl(null,[Validators.pattern(/^\d+$/)]),
+    });
+    this.getBookList();
+    this.getGenreList();
+    this.getAuthorList();
+  }
 
   getBookList(){
     this.libraryService.getBooks().subscribe(data =>{
       this.bookList = data
     })
   }
+
   getGenreList(){
     this.libraryService.getGenre().subscribe(data=>{
       this.genreList = data
     })
   }
+
   getAuthorList(){
     this.libraryService.getAuthor().subscribe(data=>{
       this.authorsList = data
@@ -44,6 +60,7 @@ export class EditBookComponent implements OnInit {
       this.displayBooks = false;
     }else this.displayBooks = true;
   }
+
   searchBook(searhBookField:string){
     this.searchBookRes.length=0;
     for(let i =0; i< this.bookList.length; i++){
@@ -59,26 +76,32 @@ export class EditBookComponent implements OnInit {
     }
   }
 
-  startEditBook( editBook:any, authorsField:any, bookField:any, genreField:any, pagesField:any){
+  startEditBook( editBook:Book, authorsField:{value: string;}, bookField: {value: string;}, genreField:{value: string;}, pagesField:{value: string;}){
     this.activationForm = false;
     this.bookForm.reset();
-    this.editingBook = editBook;
-    authorsField.value = editBook.author;
+    this.editingBook.push(editBook);
+    authorsField.value = editBook.authorId+" "+ editBook.author;
     bookField.value = editBook.title;
     genreField.value = editBook.genre;
     pagesField.value = editBook.pages;
   }
+
   finishEditBook(){
     this.activationForm = true;
-    this.editingBook.author = this.bookForm.controls.surname.value.substring(2);
-    this.editingBook.authorId = Number(this.bookForm.controls.surname.value.substring(0,1));
-    this.editingBook.title = this.bookForm.controls.title.value;
-    this.editingBook.genre = this.bookForm.controls.genre.value;
-    this.editingBook.pages = this.bookForm.controls.pages.value;
-    this.libraryService.editBook(this.editingBook).subscribe((res)=>{
-      this.editingBook = null;
-      this.getBookList();
-    })
+    if(this.bookForm.value.surname){
+      this.editingBook[0].author = this.bookForm.value.surname.substring(2);
+      this.editingBook[0].authorId = Number(this.bookForm.value.surname.substring(0,1));
+    }
+    if(this.bookForm.value.title){
+      this.editingBook[0].title = this.bookForm.value.title;
+    }
+    if(this.bookForm.value.genre){
+      this.editingBook[0].genre = this.bookForm.value.genre;
+    }
+    if(this.bookForm.value.pages){
+      this.editingBook[0].pages = this.bookForm.value.pages;
+    }
+    this.libraryService.editBook(this.editingBook[0]).subscribe()
     this.bookForm.reset();
   }
 
@@ -90,19 +113,4 @@ export class EditBookComponent implements OnInit {
     this.bookForm.reset();
   }
 
-
-
-  ngOnInit(): void {
-    this.bookForm = new FormGroup({
-      surname: new FormControl(''),
-      genre: new FormControl(''),
-      title: new FormControl('',[Validators.required,Validators.pattern(/[A-Za-z]/)]),
-      pages: new FormControl('',[Validators.required,Validators.pattern(/^\d+$/)]),
-    });
-    this.getBookList();
-    this.getGenreList();
-    this.getAuthorList();
-    
-  }
-  
 }
