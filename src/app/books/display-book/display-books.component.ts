07455475af59d90741c3c257c9cc26590598501e
ViktorSkrypnychenko <92309069+ViktorSkrypnychenko@users.noreply.github.com>
2021-12-16 10:45:54 +0200

    add type

--- src/app/books/display-book/display-books.component.ts
+++ src/app/books/display-book/display-books.component.ts
@@ -1,17 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { LibraryServiceService } from 'src/app/library-service.service';
+import { Book } from 'src/app/genre-book-author';
 
 @Component({
   selector: 'app-display-books',
   templateUrl: './display-books.component.html',
   styleUrls: ['./display-books.component.css']
 })
+
 export class DisplayBooksComponent implements OnInit {
    
-  booksList:any;
+  booksList:Book[]=[];
   
-
   constructor(private libraryService:LibraryServiceService) { }
+
+  ngOnInit(): void {
+    this.getBooksList();
+  }
 
   getBooksList(){
     this.libraryService.getBooks().subscribe(data=>{
@@ -22,15 +27,13 @@ export class DisplayBooksComponent implements OnInit {
   sortingAZ = () =>{
     return this.booksList.sort((a: any,b: any)=>a.title.localeCompare(b.title));
   }
+
   sortingAuthor = () =>{
     return this.booksList.sort((a: any,b: any)=>a.author.localeCompare(b.author));
   }
+
   sortingGenre = () =>{
     return this.booksList.sort((a: any,b: any)=>a.genre.localeCompare(b.genre));
-  }
-
-  ngOnInit(): void {
-    this.getBooksList();
   }
 
 }
