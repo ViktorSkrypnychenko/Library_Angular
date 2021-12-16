b6bcdc98b06d1f9e3d520315ec8f198a326598ec
ViktorSkrypnychenko <92309069+ViktorSkrypnychenko@users.noreply.github.com>
2021-12-16 10:48:26 +0200

    add type

--- src/app/genres/add-genre/add-genre.component.ts
+++ src/app/genres/add-genre/add-genre.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LibraryServiceService } from 'src/app/library-service.service';
 import { FormControl,Validators,FormGroup } from '@angular/forms';
+import { Genre } from 'src/app/genre-book-author';
 
 @Component({
   selector: 'app-add-genre',
@@ -9,24 +10,30 @@ import { FormControl,Validators,FormGroup } from '@angular/forms';
 })
 export class AddGenreComponent implements OnInit {
    
-  genreList:any;
-  addGenreForm:any=FormGroup;
-  alarmSearchGenreDiv = true;
-  successfull=true;
-
+  genreList:Genre[]=[];
+  addGenreForm!:FormGroup;
+  alarmSearchGenreDiv:boolean = true;
+  successfull:boolean=true;
+ 
   constructor(private libraryService:LibraryServiceService) { }
 
+  ngOnInit(): void {
+    this.addGenreForm = new FormGroup({
+      newGenre: new FormControl('',[Validators.required,Validators.pattern(/[A-Za-z]/)])
+    });
+    this.getGenre();
+  }
+  
   getGenre(){
     this.libraryService.getGenre().subscribe(data=>{
       this.genreList = data;
-      this.genreList.sort((a: any,b: any)=>a.genre.localeCompare(b.genre));
     })
   }
 
   addGenre(){
     let chekin = true;
     for (let i = 0; i < this.genreList.length; i++) {
-      if (this.genreList[i].genre == this.addGenreForm.controls.newGenre.value ){
+      if (this.genreList[i].genre == this.addGenreForm.value.newGenre ){
         this.alarmSearchGenreDiv = false;
         this.successfull = true;
         chekin = false;
@@ -34,21 +41,13 @@ export class AddGenreComponent implements OnInit {
       } 
     } 
     if(chekin == true){
-      this.libraryService.addGenre(this.addGenreForm.controls.newGenre.value).subscribe(data=>{
+        this.libraryService.addGenre(this.addGenreForm.value.newGenre).subscribe((data)=>{
         this.genreList.push(data)
-        this.getGenre();
         this.alarmSearchGenreDiv = true;
         this.successfull = false
         })
     }
     this.addGenreForm.reset();
-  }
-
-  ngOnInit(): void {
-    this.addGenreForm = new FormGroup({
-      newGenre: new FormControl('',[Validators.required,Validators.pattern(/[A-Za-z]/)])
-    });
-    this.getGenre();
   }
 
 }
