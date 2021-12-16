3868bb56fd640a36e185143201d171bff40aae2f
ViktorSkrypnychenko <92309069+ViktorSkrypnychenko@users.noreply.github.com>
2021-12-16 10:50:19 +0200

    Add files via upload

--- src/app/genres/edit-genre/edit-genre.component.ts
+++ src/app/genres/edit-genre/edit-genre.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LibraryServiceService } from 'src/app/library-service.service';
 import { FormControl,Validators,FormGroup } from '@angular/forms';
-import { ThisReceiver } from '@angular/compiler';
+import { Genre, Book,} from 'src/app/genre-book-author';
 
 @Component({
   selector: 'app-edit-genre',
@@ -10,24 +10,34 @@ import { ThisReceiver } from '@angular/compiler';
 })
 export class EditGenreComponent implements OnInit {
 
-  genreList:any;
-  addGenreForm:any=FormGroup;
-  alarmGenre:any={};
-  successfull=true;
-  searchResult = true;
-  searchResGenre:any[]=[];
-  bookList:any;
-  search=false;
-  delete=true;
-  success = true;
+  genreList:Genre[]=[];
+ 
+  successfull:boolean =true;
+  searchResult:boolean = true;
+  searchResGenre:Genre[]=[];
+  bookList:Book[]=[];
+  search:boolean = false;
+  delete:boolean = true;
+  success:boolean = true;
+  delSuccess:boolean=true;
+  addGenreForm!: FormGroup;
 
   constructor(private libraryService:LibraryServiceService) { }
+  
+  ngOnInit(): void {
+    this.addGenreForm = new FormGroup({
+      newGenre: new FormControl('',[Validators.required,Validators.pattern(/[A-Za-z]/)])
+    });
+    this.getGenre();
+    this.getBookList();
+  }
 
   getGenre(){
     this.libraryService.getGenre().subscribe(data=>{
       this.genreList = data;
     })
   }
+
   getBookList(){
     this.libraryService.getBooks().subscribe(data =>{
       this.bookList = data
@@ -35,11 +45,12 @@ export class EditGenreComponent implements OnInit {
   }
 
   searchGenre(){
-    this.success = true
-    this.searchResult = false
+    this.delSuccess = true;
+    this.success = true;
+    this.searchResult = false;
     for (let i = 0; i < this.genreList.length; i++) {
       this.searchResGenre.length = 0
-      if (this.genreList[i].genre == this.addGenreForm.controls.newGenre.value ){
+      if (this.genreList[i].genre == this.addGenreForm.value.newGenre){
         this.searchResGenre.push(this.genreList[i])
         this.search=true;
         this.delete=false;
@@ -47,9 +58,7 @@ export class EditGenreComponent implements OnInit {
       }
     }
     if (this.searchResGenre.length<=0){
-      
-      this.alarmGenre = {genre:"There is no this genre"};
-      this.searchResGenre.push(this.alarmGenre);
+      this.searchResGenre.push({id:1,genre:"There is no this genre"});
       this.search=false;
       this.delete=true;
       this.addGenreForm.reset();
@@ -57,18 +66,14 @@ export class EditGenreComponent implements OnInit {
   }
 
   finishEditingGenre(){
-    this.searchResGenre[0].genre = this.addGenreForm.controls.newGenre.value
-    this.libraryService.editGenre(this.searchResGenre[0]).subscribe(data=>{
-      this.getGenre();
-     })
+    this.searchResGenre[0].genre = this.addGenreForm.value.newGenre;
+    this.libraryService.editGenre(this.searchResGenre[0]).subscribe();
     this.bookList.forEach((el:any) => {
       if(el.genre!=this.searchResGenre[0].genre && el.genreId==this.searchResGenre[0].id){
          el.genre = this.searchResGenre[0].genre;
-         this.libraryService.editBook(el).subscribe((res)=>{
-        })
+         this.libraryService.editBook(el).subscribe();
       }
-      this.getBookList();
-     });
+    });
     this.addGenreForm.reset();
     this.delete = true;
     this.search = false;
@@ -76,18 +81,14 @@ export class EditGenreComponent implements OnInit {
     this.searchResult = true;
   }
 
-  deleteGenre(id: any){
-    this.libraryService.deleteGenre(id).subscribe(del=>{
-      this.getBookList();
-    })
-  }
-
-  ngOnInit(): void {
-    this.addGenreForm = new FormGroup({
-      newGenre: new FormControl('',[Validators.required,Validators.pattern(/[A-Za-z]/)])
-    });
-    this.getGenre();
-    this.getBookList();
+  deleteGenre(id: number){
+    this.libraryService.deleteGenre(id).subscribe();
+    this.delete = true;
+    this.search = false;
+    this.searchResult = true;
+    this.success = true;
+    this.delSuccess = false;
+    this.addGenreForm.reset();
   }
 
 }
