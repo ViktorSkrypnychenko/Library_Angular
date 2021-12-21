93661e7e68d7efbaf2237f1f42c07738e30c4ebf
ViktorSkrypnychenko <92309069+ViktorSkrypnychenko@users.noreply.github.com>
2021-12-21 13:38:12 +0200

    Add files via upload

---
+++ src/app/Authors/display-authors/display-authors.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { LibraryServiceService } from 'src/app/library-service.service';
+import { Author,Book } from 'src/app/types';
+
+@Component({
+  selector: 'app-display-authors',
+  templateUrl: './display-authors.component.html',
+  styleUrls: ['./display-authors.component.css']
+})
+export class DisplayAuthorsComponent implements OnInit {
+
+  authorsList:Author[]=[];
+  displayAuthorBooks:Book[]=[];
+  displayBooksDiv:boolean = true;
+  booksList:Book[]=[];
+
+  constructor(private libraryService:LibraryServiceService) { }
+
+  ngOnInit(): void {
+    this.getAuthorsList();
+    this.getBooksList();
+  }
+  
+  getAuthorsList(){
+    this.libraryService.getAuthor().subscribe(data => {
+      this.authorsList = data;
+      this.authorsList.sort((a: any,b: any)=>a.surname.localeCompare(b.surname));
+    })
+  }
+  
+  getBooksList(){
+    this.libraryService.getBooks().subscribe(data=>{
+      this.booksList = data;
+    })
+  }
+  
+  displayBooks(id:number){
+    this.displayAuthorBooks.length = 0;
+    this.displayBooksDiv = false;
+    this.booksList.forEach((element: any)=>{
+      if (element.authorId == id){
+        this.displayAuthorBooks.push(element)
+      }
+    });
+  }
+
+}
