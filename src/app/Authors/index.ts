be27ba4c94c5ee6ab30036599caf139b9ea0f16b
ViktorSkrypnychenko <92309069+ViktorSkrypnychenko@users.noreply.github.com>
2021-12-21 13:35:05 +0200

    Add files via upload

---
+++ src/app/Authors/index.ts
@@ -0,0 +1,3 @@
+export * from "./add-authors/add-authors.component";
+export * from "./display-authors/display-authors.component";
+export * from "./edit-authors/edit-authors.component";
---
+++ src/app/Authors/routs.ts
@@ -0,0 +1,7 @@
+import { AddAuthorsComponent,EditAuthorsComponent,DisplayAuthorsComponent } from "./index";
+
+export const authorsRoute = [
+    {path: "add-authors", component: AddAuthorsComponent},
+    {path: "edit-authors", component: EditAuthorsComponent},
+    {path: "display-authors", component:DisplayAuthorsComponent}
+];
