e810d9e1b3db5f38b6683907981d335c38c1b107
ViktorSkrypnychenko <92309069+ViktorSkrypnychenko@users.noreply.github.com>
2021-12-21 13:43:44 +0200

    Add files via upload

---
+++ src/app/Books/display-books/display-books.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { LibraryServiceService } from 'src/app/library-service.service';
+import { Book } from 'src/app/types';
+
+@Component({
+  selector: 'app-display-books',
+  templateUrl: './display-books.component.html',
+  styleUrls: ['./display-books.component.css']
+})
+export class DisplayBooksComponent implements OnInit {
+
+  booksList:Book[]=[];
+
+  constructor(private libraryService:LibraryServiceService) { }
+
+  ngOnInit(): void {
+    this.getBooksList();
+  }
+
+  getBooksList(){
+    this.libraryService.getBooks().subscribe(data=>{
+      this.booksList = data;
+    })
+  }
+
+  sortingAZ = () =>{
+    return this.booksList.sort((a: any,b: any)=>a.title.localeCompare(b.title));
+  }
+
+  sortingAuthor = () =>{
+    return this.booksList.sort((a: any,b: any)=>a.author.localeCompare(b.author));
+  }
+
+  sortingGenre = () =>{
+    return this.booksList.sort((a: any,b: any)=>a.genre.localeCompare(b.genre));
+  }
+
+}
